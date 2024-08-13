19df144b2247ef4899c75e6c96696735a45fc1e7
Omar Fetooh <100229382+Omar-Fetooh@users.noreply.github.com>
2024-08-13 23:02:30 +0300

    [1482] [Time Beats: 81.39%] [Memory Beats: 13.47%] - LeetPush

--- Data structures and algorithms/1482-Minimum-Number-of-Days-to-Make-m-Bouquets.cpp
+++ Data structures and algorithms/1482-Minimum-Number-of-Days-to-Make-m-Bouquets.cpp
@@ -3,18 +3,13 @@ public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n=bloomDay.size();
         if((double)m*k>n)return -1;
-        unordered_set<int>unique_elements(bloomDay.begin(),bloomDay.end());
-        vector<int>searchSpace;
-        for(auto element:unique_elements){
-            searchSpace.push_back(element);
-        }
-        sort(searchSpace.begin(),searchSpace.end());
-        int low=0,high=searchSpace.size()-1;
+        int low=*min_element(bloomDay.begin(),bloomDay.end());
+        int high=*max_element(bloomDay.begin(),bloomDay.end());
         while(low<=high){
             int mid=low+(high-low)/2;
             int bouquetsDone=0,tempk=k;
             for(int i=0;i<n;i++){
-                if(bloomDay[i]<=searchSpace[mid]){
+                if(bloomDay[i]<=mid){
                     tempk--;
                     if(tempk==0){
                         tempk=k;
@@ -26,6 +21,6 @@ public:
             if(bouquetsDone>=m) high=mid-1;
             else low=mid+1;
         }
-        return searchSpace[low];
+        return low;
     }
 };
