1ebfd989812a2f4133a9f162e95effac8173c0d2
Omar Fetooh <100229382+Omar-Fetooh@users.noreply.github.com>
2024-08-27 01:12:12 +0300

    [1614] [Time Beats: 40.66%] [Memory Beats: 61.56%] - LeetPush

---
+++ Data structures and algorithms/1614-Maximum-Nesting-Depth-of-the-Parentheses.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt=0,maxi=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')++cnt;
+            else if(s[i]==')')--cnt;
+            maxi=max(maxi,cnt);
+        }
+        return maxi;
+    }
+};
