2de2fb28a5bc0fa4a99b1acbac9390748db4607a
Omar Fetooh <100229382+Omar-Fetooh@users.noreply.github.com>
2025-01-10 00:49:56 +0200

    [136] [Time Beats: 100.00%] [Memory Beats: 26.06%] - LeetPush

--- Data structures and algorithms/136-Single-Number.cpp
+++ Data structures and algorithms/136-Single-Number.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        sort(nums.begin(),nums.end());
-        int ans;
-          int cnt=1;
-        for(int i=0;i<nums.size();i++){
-          if(nums[i]==nums[i+1]) ++cnt;
-          else if(nums[i]!=nums[i+1]&&cnt==2){
-            cnt=1;
-          }
-          else{
-                  ans=nums[i];
-                break;
-          }
-        }
-        return ans;
+      int Xorr=0;
+      for(int i=0;i<nums.size();i++){
+        Xorr=Xorr^nums[i];
+      }
+
+      return Xorr;
     }
 };
