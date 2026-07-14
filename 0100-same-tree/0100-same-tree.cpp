0fdad87e55f760e9502eb94b95832afcc35aebfa
Omar Fetooh <100229382+Omar-Fetooh@users.noreply.github.com>
2026-07-14 21:52:31 +0300

    Time: 0 ms (100%), Space: 12.7 MB (78.16%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,40 +12,17 @@
  */
 class Solution {
 public:
-    vector<int> vp;
-    vector<int> vq;
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        vp.clear();
-        vq.clear();
-        dfs(p);
-        dfs2(q);
-        if (vp.size() != vq.size())
+    bool DFS(TreeNode* p, TreeNode* q) {
+        if (p == q)
+            return true;
+        if (!p || !q || p->val != q->val)
             return false;
-        for (int i = 0; i < vp.size(); i++) {
-            if (vp[i] != vq[i])
-                return false;
-        }
-        return true;
+
+        bool left = DFS(p->left, q->left);
+        bool right = DFS(p->right, q->right);
+
+        return left && right;
     }
 
-    TreeNode* dfs(TreeNode* p) {
-        if (!p) {
-            vp.push_back(-100000);
-            return NULL;
-        }
-        vp.push_back(p->val);
-        dfs(p->left);
-        dfs(p->right);
-        return nullptr;
-    }
-    TreeNode* dfs2(TreeNode* p) {
-        if (!p) {
-            vq.push_back(-100000);
-            return NULL;
-        }
-        vq.push_back(p->val);
-        dfs2(p->left);
-        dfs2(p->right);
-        return nullptr;
-    }
+    bool isSameTree(TreeNode* p, TreeNode* q) { return DFS(p, q); }
 };
