806d0cd389817c415c319d60263c96884136f307
Omar Fetooh <100229382+Omar-Fetooh@users.noreply.github.com>
2026-07-18 13:28:45 +0300

    Time: 3 ms (2.21%), Space: 18.6 MB (32.96%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,45 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    bool isSymmetricHelp(TreeNode* left, TreeNode* right) {
+        if (left == NULL || right == NULL)
+            return left == right;
+        if (left->val != right->val)
+            return false;
 
-    void preOrder(TreeNode* node, vector<int>& leftTree){
-        if(node==NULL) {
-            leftTree.push_back(-1000);
-            return;
-        }
-        leftTree.push_back(node->val);
-        preOrder(node->left,leftTree);
-        preOrder(node->right,leftTree);
-    }
-
-    void revPreOrder(TreeNode* node, vector<int>& rightTree){
-        if(node==NULL) {
-            rightTree.push_back(-1000);
-            return;
-        }
-        rightTree.push_back(node->val);
-        revPreOrder(node->right,rightTree);
-        revPreOrder(node->left,rightTree);
+        return isSymmetricHelp(left->left, right->right) &&
+               isSymmetricHelp(left->right, right->left);
     }
 
     bool isSymmetric(TreeNode* root) {
-        vector<int> leftTree;
-        vector<int> rightTree;
-
-        preOrder(root->left,leftTree);
-        revPreOrder(root->right,rightTree);
-
-        if(leftTree.size()!= rightTree.size()) return false;
-
-        for(int i=0;i<leftTree.size();i++){
-            if(leftTree[i]!=rightTree[i]) return false;
-        }
-
-        return true;
+        return isSymmetricHelp(root->left, root->right);
     }
 };
